2bde871c04a47deb45b951d845760641e5c66bb5
yanyabing1 <457958667@qq.com>
2020-10-12 13:35:22 +0800

    Updates

---
+++ precache-manifest.3d898f5a0cd8ada080a5b6b789506fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68761935b4e3b71079e66155c4b0b17",
+    "url": "/yanyabing1/ali/index.html"
+  },
+  {
+    "revision": "74c5dc6f93f8a52f1d90",
+    "url": "/yanyabing1/ali/static/css/main.4f7dc9f2.chunk.css"
+  },
+  {
+    "revision": "40ee4ff5fc62c613e9bc",
+    "url": "/yanyabing1/ali/static/js/2.1558b5b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yanyabing1/ali/static/js/2.1558b5b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74c5dc6f93f8a52f1d90",
+    "url": "/yanyabing1/ali/static/js/main.a3147dc3.chunk.js"
+  },
+  {
+    "revision": "0c02f75e906ad7cd4942",
+    "url": "/yanyabing1/ali/static/js/runtime-main.800b6e26.js"
+  }
+]);
